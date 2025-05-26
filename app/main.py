cb1c650db0f3fcea00cf146d61dcb2b6c2fafb29
Sujit Hulule <99439062+Beyonder016@users.noreply.github.com>
2025-05-26 16:13:04 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
 # app/main1.py
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 
 import streamlit as st
 from app.spatial_logic import Device, check_connections
